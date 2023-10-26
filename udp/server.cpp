07772bf7c53cc618dfda1c72f347c19bdc3779f7
kangjung-hwa <65651930+kangjung-hwa@users.noreply.github.com>
2023-10-26 14:10:32 +0900

    Update server.cpp

--- udp/server.cpp
+++ udp/server.cpp
@@ -1,55 +1,43 @@
 #include <iostream>
 #include <cstring>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 int main() {
-    // 서버의 IP 주소와 포트 설정
-    const char* server_ip = "0.0.0.0";  // 모든 네트워크 인터페이스에서 수신
-    const int server_port = 12345;
-
-    // 소켓 생성
-    int server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_socket == -1) {
-        std::cerr << "소켓을 생성할 수 없습니다." << std::endl;
+    // Create a UDP socket
+    int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udpSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
         return -1;
     }
 
-    sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-
-    // 소켓을 주소와 포트에 바인딩
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "바인딩에 실패했습니다." << std::endl;
-        close(server_socket);
+    // Enable UDP broadcast
+    int enableBroadcast = 1;
+    if (setsockopt(udpSocket, SOL_SOCKET, SO_BROADCAST, &enableBroadcast, sizeof(enableBroadcast)) == -1) {
+        std::cerr << "Error setting broadcast option" << std::endl;
         return -1;
     }
 
-    std::cout << "서버가 " << server_ip << ":" << server_port << "에서 대기 중입니다." << std::endl;
+    // Server address information
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(12345); // Port number
+    serverAddress.sin_addr.s_addr = INADDR_BROADCAST;
 
-    char buffer[1024];
-    sockaddr_in client_addr;
-    socklen_t addr_len = sizeof(client_addr);
+    char message[1024];
 
     while (true) {
-        int bytes_received = recvfrom(server_socket, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, &addr_len);
+        // Get user input
+        std::cout << "Server: ";
+        std::cin.getline(message, sizeof(message));
 
-        if (bytes_received > 0) {
-            buffer[bytes_received] = '\0';
-            std::cout << "클라이언트: " << buffer << std::endl;
-
-            // 서버로부터 메시지 입력
-            std::cout << "서버: ";
-            std::cin.getline(buffer, sizeof(buffer));
-
-            // 클라이언트에게 메시지 보내기
-            sendto(server_socket, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, addr_len);
-        }
+        // Send a broadcast message
+        sendto(udpSocket, message, strlen(message), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
     }
 
-    close(server_socket);
+    // Close the socket
+    close(udpSocket);
+
     return 0;
 }
